ae5650dd4f954e375ff3ab5b8005806f559c6d91
Volodya22211 <Volodymyr.Baran.RI.2023@lpnu.ua>
2023-10-24 21:51:08 +0300

    Add project files.

---
+++ ConsoleApplication1/Lab 5.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double k(const double a, const double b); // прототип
+int main()
+{
+	double p, q;
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+	double g = pow(k(p + sqrt(q), q - sqrt(p)), 2) - k(1, p + q);
+	cout << "g = " << g << endl;
+	return 0;
+}
+double k(const double x, const double y) // визначення
+{
+	return (x / abs(pow(x, 3) + pow(y, 3)) + y / (abs(x + y)));
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ConsoleApplication1/Lab 5.1.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double u;
+			u = k(2, 3);
+			Assert::AreEqual(u, 0.657143, 0.000001);
+		}
+	};
+}
